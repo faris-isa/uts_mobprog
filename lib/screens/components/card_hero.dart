3854ad8be9944e494a0aa9e51e1517c6c5c015ca
faris-isa <m.faris.isa@gmail.com>
2023-06-06 16:31:27 +0700

    add clickable detail hero

--- lib/screens/components/card_hero.dart
+++ lib/screens/components/card_hero.dart
@@ -4,6 +4,7 @@ import 'package:uts_mobprog/data/model/heroes.dart';
 import 'package:uts_mobprog/provider/pick_provider.dart';
 
 import '../../data/model/heroes_merge.dart';
+import '../hero_detail_page.dart';
 
 class CardHero extends StatefulWidget {
   final HeroesMerge hero;
@@ -23,50 +24,57 @@ class _CardHeroState extends State<CardHero> {
   Widget build(BuildContext context) {
     return AspectRatio(
       aspectRatio: 2 / 3,
-      child: Container(
-        margin: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 10.0),
-        decoration: BoxDecoration(
-          border: Border.all(
-            color: widget.hero.primaryAttr == PrimaryAttr.ALL 
-              ? Colors.yellow[400]! 
-                : widget.hero.primaryAttr == PrimaryAttr.STR 
-                  ? Colors.red
-                    : widget.hero.primaryAttr == PrimaryAttr.AGI 
-                      ? Colors.green[400]!
-                        : Colors.blue[600]! , 
-            width: 4.0),
-          borderRadius: BorderRadius.circular(20),
-          image: DecorationImage(
-            fit: BoxFit.cover,
-            alignment: Alignment.center,
-            image: NetworkImage('https://api.opendota.com${widget.hero.icon}')
-          )
+      child: GestureDetector(
+         onTap: () => Navigator.pushNamed(
+          context, 
+          HeroDetailPage.routeName,
+          arguments: widget.hero
         ),
         child: Container(
+          margin: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 10.0),
           decoration: BoxDecoration(
+            border: Border.all(
+              color: widget.hero.primaryAttr == PrimaryAttr.ALL 
+                ? Colors.yellow[400]! 
+                  : widget.hero.primaryAttr == PrimaryAttr.STR 
+                    ? Colors.red
+                      : widget.hero.primaryAttr == PrimaryAttr.AGI 
+                        ? Colors.green[400]!
+                          : Colors.blue[600]! , 
+              width: 4.0),
             borderRadius: BorderRadius.circular(20),
-            gradient: LinearGradient(
-              begin: Alignment.bottomRight,
-              stops: const [0.1, 0.9],
-              colors: widget.hero.primaryAttr == PrimaryAttr.ALL 
-              ? [Colors.yellow[700]!.withOpacity(.9), Colors.yellow[200]!.withOpacity(.2) ] 
-                : widget.hero.primaryAttr == PrimaryAttr.STR 
-                  ? [Colors.red[800]!.withOpacity(.9), Colors.red[200]!.withOpacity(.2) ] 
-                    : widget.hero.primaryAttr == PrimaryAttr.AGI 
-                      ? [Colors.green[900]!.withOpacity(.9), Colors.green[200]!.withOpacity(.2) ] 
-                        : [Colors.blue[900]!.withOpacity(.9), Colors.blue[200]!.withOpacity(.2) ] 
+            image: DecorationImage(
+              fit: BoxFit.cover,
+              alignment: Alignment.center,
+              image: NetworkImage('https://api.opendota.com${widget.hero.icon}')
             )
           ),
-          child: Align(
-            alignment: Alignment.bottomLeft,
-            child: Padding(
-              padding: const EdgeInsets.only(left: 6.0, bottom: 18.0),
-              child: Text(
-                widget.hero.localizedName, 
-                style: TextStyle(
-                  color: widget.hero.primaryAttr == PrimaryAttr.ALL ? Colors.black : Colors.white,
-                  fontSize: 12
-                )
+          child: Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              gradient: LinearGradient(
+                begin: Alignment.bottomRight,
+                stops: const [0.1, 0.9],
+                colors: widget.hero.primaryAttr == PrimaryAttr.ALL 
+                ? [Colors.yellow[700]!.withOpacity(.9), Colors.yellow[200]!.withOpacity(.2) ] 
+                  : widget.hero.primaryAttr == PrimaryAttr.STR 
+                    ? [Colors.red[800]!.withOpacity(.9), Colors.red[200]!.withOpacity(.2) ] 
+                      : widget.hero.primaryAttr == PrimaryAttr.AGI 
+                        ? [Colors.green[900]!.withOpacity(.9), Colors.green[200]!.withOpacity(.2) ] 
+                          : [Colors.blue[900]!.withOpacity(.9), Colors.blue[200]!.withOpacity(.2) ] 
+              )
+            ),
+            child: Align(
+              alignment: Alignment.bottomLeft,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 6.0, bottom: 18.0),
+                child: Text(
+                  widget.hero.localizedName, 
+                  style: TextStyle(
+                    color: widget.hero.primaryAttr == PrimaryAttr.ALL ? Colors.black : Colors.white,
+                    fontSize: 12
+                  )
+                ),
               ),
             ),
           ),
--- lib/screens/hero_detail_page.dart
+++ lib/screens/hero_detail_page.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:html/parser.dart';
+import 'package:provider/provider.dart';
+import 'package:uts_mobprog/data/model/heroes.dart';
 
 import '../data/model/heroes_merge.dart';
+import '../provider/pick_provider.dart';
 
 
 
-class HeroDetailPage extends StatelessWidget {
+class HeroDetailPage extends StatefulWidget {
   static const routeName = '/hero_detail';
 
   final HeroesMerge hero;
@@ -13,32 +15,130 @@ class HeroDetailPage extends StatelessWidget {
   const HeroDetailPage(this.hero);
 
   @override
+  State<HeroDetailPage> createState() => _HeroDetailPageState();
+}
+
+class _HeroDetailPageState extends State<HeroDetailPage> {
+  bool _isSelected = false;
+
+  @override
   Widget build(BuildContext context) {
-    String aliasesName = hero.aliases.map((aliases) => aliases).join(', ');
+    String aliasesName = widget.hero.aliases.map((aliases) => aliases).join(', ');
 
     return Scaffold(
-      backgroundColor: Colors.grey,
+      // backgroundColor: Colors.grey,
       appBar: AppBar(
         backgroundColor: Colors.black,
-        title: Text(hero.localizedName),
+        title: Text(widget.hero.localizedName),
+        actions: <Widget>[
+          IconButton(
+            icon: new Icon(Icons.contacts, color: Colors.white), 
+            onPressed:() {
+              Navigator.pushNamed(context, '/pick_hero');
+            }
+          ),
+        ],
       ),
       body: SingleChildScrollView(
         child: Column(
           children: [
-            Container(
-              alignment: Alignment.centerLeft,
-              width: MediaQuery.of(context).size.width,
-              height: 200,
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: NetworkImage('https://api.opendota.com${hero.img}'),
-                  fit: BoxFit.cover
+            Stack(
+              alignment: Alignment.bottomCenter,
+              children: [
+                Container(
+                  alignment: Alignment.centerLeft,
+                  width: MediaQuery.of(context).size.width,
+                  margin: const EdgeInsets.only(bottom: 20),
+                  height: 200,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: NetworkImage('https://api.opendota.com${widget.hero.img}'),
+                      fit: BoxFit.cover
+                    )
+                  ),
+                  child: Hero(
+                    tag: widget.hero.name,
+                    child: const SizedBox.shrink(),
+                  ),
+                ),
+                Positioned(
+                  bottom: -5,
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                    decoration: const BoxDecoration(
+                      color: Colors.white
+                    ),
+                    child: Row (
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                         Container(
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              color: (widget.hero.primaryAttr == PrimaryAttr.STR || widget.hero.primaryAttr == PrimaryAttr.ALL ) ? Colors.red[600]! : Colors.white,
+                              width: 5.0,
+                            ),
+                            borderRadius: BorderRadius.circular(50)
+                          ),
+                          child: Image.network(
+                            'https://static.wikia.nocookie.net/dota2_gamepedia/images/7/7a/Strength_attribute_symbol.png',
+                            width: 30,
+                            height: 30,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                        const SizedBox(
+                          width: 5,
+                        ),
+                        Text('${widget.hero.stat.strengthBase.toStringAsFixed(0)} + ${widget.hero.stat.strengthGain.toStringAsFixed(1)}', style: const TextStyle(fontSize: 18)),
+                        const SizedBox(
+                          width: 25,
+                        ),
+                        Container(
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              color: (widget.hero.primaryAttr == PrimaryAttr.AGI || widget.hero.primaryAttr == PrimaryAttr.ALL ) ? Colors.green : Colors.white,
+                              width: 5.0,
+                            ),
+                            borderRadius: BorderRadius.circular(50)
+                          ),
+                          child: Image.network(
+                            'https://static.wikia.nocookie.net/dota2_gamepedia/images/2/2d/Agility_attribute_symbol.png',
+                            width: 30,
+                            height: 30,
+                            fit: BoxFit.cover
+                          ),
+                        ),
+                        const SizedBox(
+                          width: 5,
+                        ),
+                        Text('${widget.hero.stat.agilityBase.toStringAsFixed(0)} + ${widget.hero.stat.agilityGain.toStringAsFixed(1)}', style: const TextStyle(fontSize: 18)),
+                        const SizedBox(
+                          width: 25,
+                        ),
+                        Container(
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              color: (widget.hero.primaryAttr == PrimaryAttr.INT || widget.hero.primaryAttr == PrimaryAttr.ALL ) ? Colors.blue[300]! : Colors.white,
+                              width: 5.0,
+                            ),
+                            borderRadius: BorderRadius.circular(50)
+                          ),
+                          child: Image.network(
+                            'https://static.wikia.nocookie.net/dota2_gamepedia/images/5/56/Intelligence_attribute_symbol.png',
+                            width: 30,
+                            height: 30,
+                            fit: BoxFit.cover
+                          ),
+                        ),
+                        const SizedBox(
+                          width: 5,
+                        ),
+                        Text('${widget.hero.stat.intelligenceBase.toStringAsFixed(0)} + ${widget.hero.stat.intelligenceGain.toStringAsFixed(1)}', style: const TextStyle(fontSize: 18))
+                      ],
+                    ),
+                  )
                 )
-              ),
-              child: Hero(
-                tag: hero.name,
-                child: const SizedBox.shrink(),
-              ),
+              ],
             ),
             Padding(
               padding: const EdgeInsets.all(10),
@@ -48,12 +148,12 @@ class HeroDetailPage extends StatelessWidget {
                   Row (
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      Image.network('https://api.opendota.com${hero.icon}'),
+                      Image.network('https://api.opendota.com${widget.hero.icon}'),
                       const SizedBox(
                         width: 10,
                       ),
                       Text(
-                        hero.localizedName,
+                        widget.hero.localizedName,
                         style: Theme.of(context).textTheme.bodyMedium,
                       ),
                     ],
@@ -61,61 +161,118 @@ class HeroDetailPage extends StatelessWidget {
                   const SizedBox(
                     height: 10,
                   ),
-                  Row (
-                    mainAxisAlignment: MainAxisAlignment.center,
+                  Stack(
+                    alignment: Alignment.topCenter,
                     children: [
-                      Image.network(
-                        'https://static.wikia.nocookie.net/dota2_gamepedia/images/7/7a/Strength_attribute_symbol.png',
-                        width: 30,
-                        height: 30,
-                        fit: BoxFit.cover
+                      Container(
+                        margin: const EdgeInsets.symmetric(horizontal: 26.0, vertical: 10),
+                        padding: const EdgeInsets.only(top: 20, left: 10, right: 10, bottom: 10),
+                        decoration: const BoxDecoration(
+                          color: Colors.grey,
+                          boxShadow: [
+                            BoxShadow(
+                              blurRadius: 5,
+                              offset: Offset(0, 5)
+                            ),
+                            BoxShadow(
+                              color: Colors.grey,
+                              offset: Offset(-10, 0)
+                            ),
+                            BoxShadow(
+                              color: Colors.grey,
+                              offset: Offset(10, 0)
+                            ),
+                          ]
+                          ),
+                        child: Text(
+                          widget.hero.language.bio,
+                          textAlign: TextAlign.justify,
+                          style: const TextStyle(color: Colors.white, fontSize: 12),
+                        ),
                       ),
-                      const SizedBox(
-                        width: 5,
+                      Positioned(
+                        child: Container(
+                          padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 20),
+                          decoration : const BoxDecoration(
+                            color: Colors.black
+                          ),
+                          child: const Text("Biodata", style: TextStyle(color: Colors.white)),
+                        ),
                       ),
-                      Text('${hero.stat.strengthBase.toStringAsFixed(0)} + ${hero.stat.strengthGain.toStringAsFixed(1)}'),
-                      const SizedBox(
-                        width: 25,
-                      ),
-                      Image.network(
-                        'https://static.wikia.nocookie.net/dota2_gamepedia/images/2/2d/Agility_attribute_symbol.png',
-                        width: 30,
-                        height: 30,
-                        fit: BoxFit.cover
-                      ),
-                      const SizedBox(
-                        width: 5,
-                      ),
-                      Text('${hero.stat.agilityBase.toStringAsFixed(0)} + ${hero.stat.agilityGain.toStringAsFixed(1)}'),
-                      const SizedBox(
-                        width: 25,
-                      ),
-                      Image.network(
-                        'https://static.wikia.nocookie.net/dota2_gamepedia/images/5/56/Intelligence_attribute_symbol.png',
-                        width: 30,
-                        height: 30,
-                        fit: BoxFit.cover
-                      ),
-                      const SizedBox(
-                        width: 5,
-                      ),
-                      Text('${hero.stat.intelligenceBase.toStringAsFixed(0)} + ${hero.stat.intelligenceGain.toStringAsFixed(1)}')
                     ],
                   ),
-                  const Divider(color: Colors.grey),
-                  Text(
-                    hero.language.bio,
-                    textAlign: TextAlign.justify,
-                    style: const TextStyle(color: Colors.white, fontSize: 12),
+                  SizedBox(
+                    height: 10,
                   ),
-                  const Divider(color: Colors.grey),
                   Text(
                     'Alias : $aliasesName',
                     style: Theme.of(context).textTheme.bodySmall,
                   ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Container(
+                    margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+                    child: Row(
+                      children: [
+                        Consumer<PickProvider>(
+                          builder: (context, data, child) {
+                            var isPicked = data.radiantTeamPick.contains(widget.hero);
+                            var isDirePicked = data.direTeamPick.contains(widget.hero);
+                            return ElevatedButton(
+                              onPressed:  (data.radiantTeamPick.length < 5 && !isPicked && !isDirePicked) ? () {
+                                setState(() {
+                                  data.addRadiant(widget.hero);
+                                  _isSelected = !_isSelected;
+                                });
+                              } : null,
+                              style:  (data.radiantTeamPick.length < 5 && !isPicked && !isDirePicked)
+                              ? ElevatedButton.styleFrom(
+                                backgroundColor: Colors.greenAccent,
+                                minimumSize: Size(175, 50)
+                              ) 
+                              : ElevatedButton.styleFrom(
+                                backgroundColor: Colors.grey,
+                                minimumSize: Size(175, 50)
+                              ), 
+                              child: const Text('Radiant', style: TextStyle(color: Colors.black),),
+                            );
+                          },
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Consumer<PickProvider>(
+                          builder: (context, data, child) {
+                            var isPicked = data.direTeamPick.contains(widget.hero);
+                            var isRadiantPicked = data.radiantTeamPick.contains(widget.hero);
+                            return ElevatedButton(
+                              onPressed: (data.direTeamPick.length < 5 && !isPicked && !isRadiantPicked) ? () {
+                                setState(() {
+                                  data.addDire(widget.hero);
+                                  _isSelected = !_isSelected;
+                                });
+                              } : null,
+                              style: (data.direTeamPick.length < 5 && !isPicked && !isRadiantPicked)
+                              ? ElevatedButton.styleFrom(
+                                backgroundColor: Colors.redAccent,
+                                minimumSize: Size(175, 50)
+                              ) 
+                              : ElevatedButton.styleFrom(
+                                backgroundColor: Colors.grey,
+                                minimumSize: Size(175, 50)
+                              ), 
+                              child: const Text('Dire', style: TextStyle(color: Colors.black),),
+                            );
+                          },
+                        )
+                      ],
+                    ),
+                  )
                 ],
               ),
             ),
+
           ],
         ),
       ),
